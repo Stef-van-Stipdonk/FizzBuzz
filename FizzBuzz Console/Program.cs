c3b770a7b46811d1b826dcd5fcbaba9640651dd6
Stef Stipdonk <stef.vanstipdonk@edu-kw1c.nl>
2020-11-03 20:20:27 +0100

    Added Second solution

--- FizzBuzz Console/Program.cs
+++ FizzBuzz Console/Program.cs
@@ -13,28 +13,43 @@ namespace FizzBuzz_Console
              * But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
              * For numbers which are multiples of both three and five print “FizzBuzz”.
              */
-            Console.WriteLine("Choose a solution");
-
-            List<string> list = new List<string> { "1: First Solution", "2: Secound Solution" };
-
-            for (int i = 0; i < list.Count; i++)
+            var x = true;
+            while (x)
             {
-                Console.WriteLine(list[i]);
+                Console.WriteLine("Choose a solution");
+
+                List<string> list = new List<string> { "1: First Solution", "2: Second Solution" };
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine(list[i]);
+                }
+
+                int solutionNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Choose a FizzBuzz number");
+                int chosenRunNumber = Convert.ToInt32(Console.ReadLine());
+                switch (solutionNumber)
+                {
+                    case 1:
+                        FirstSolution.Solution(chosenRunNumber);
+                        break;
+                    case 2:
+                        SecSolution.Solution(chosenRunNumber);
+                        break;
+                }
+
+                Console.WriteLine("Do you want to try another solution.\n Yes = Y\n No = N");
+                string answer = Console.ReadLine();
+                Console.Clear();
+                if (answer.ToLower() == "n")
+                {
+                    x = false;
+                }
+
             }
 
-            int solutionNumber = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Choose a FizzBuzz number");
-            int chosenRunNumber = Convert.ToInt32(Console.ReadLine());
-            switch (solutionNumber)
-            {
-                case 1:
-                    FirstSolution.Solution(chosenRunNumber);
-                    break;
-                case 2:
-                    SecSolution.Solution(chosenRunNumber);
-                    break;
-            }
+            
         }
     }
 }
--- FizzBuzz Console/SecSolution.cs
+++ FizzBuzz Console/SecSolution.cs
@@ -13,7 +13,33 @@ namespace FizzBuzz_Console
              * But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
              * For numbers which are multiples of both three and five print “FizzBuzz”.
              */
+            var runTime = System.Diagnostics.Stopwatch.StartNew();
 
+            for (int i = 0; i < number; i++)
+            {
+                string x = "";
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    x += "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    x += "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    x += "Buzz";
+                }
+                else
+                {
+                    x += i.ToString();
+                }
+                Console.WriteLine(x);
+            }
+
+            runTime.Stop();
+
+            Console.WriteLine("\nElapsed runtime " + runTime.ElapsedMilliseconds);
         }
     }
 }
